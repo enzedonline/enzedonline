404fb66f33be4d347ace9bdf0f240607bf14aaed
Richard Allen <richard@enzedonline.com>
2021-07-12 18:22:33 +0200

    Aborting java commenting

--- enzedonline/urls.py
+++ enzedonline/urls.py
@@ -1,16 +1,17 @@
+from core.views import RobotsView
 from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
+from django.views import defaults as default_views
 from menu.views import set_language_from_url
 from search import views as search_views
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
-from core.views import RobotsView
-from django.views import defaults as default_views
+
 
 def trigger_error(request):
     division_by_zero = 1 / 0
@@ -23,6 +24,7 @@ urlpatterns = [
     url(r'^sitemap.xml$', sitemap),
     url(r'^comments/', include('django_comments_xtd.urls')),
     path('sentry-debug/', trigger_error),
+
     # path('lang/<str:language_code>/', set_language_from_url, name='set_language_from_url'),
     # Creates urls like yourwebsite.com/login/
     url(r'', include('allauth.urls')),
