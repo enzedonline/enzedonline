3dcb6169f37e5a14ab5ac24415345649a356a23a
Richard Allen <richard@enzedonline.com>
2021-08-02 18:29:12 +0200

    Finish search, add to menu. Tidy menu.

--- enzedonline/static/js/enzedonline.js
+++ enzedonline/static/js/enzedonline.js
@@ -17,3 +17,23 @@ window.fbAsyncInit = function() {
      js.src = "https://connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
+
+
+  //  Dropdown menu aligns with right of button, lose focus after click
+       $(document).ready(function() {
+           $('.dropright button').on("click", function(e) {
+               e.stopPropagation();
+               e.preventDefault();
+   
+               if (!$(this).next('div').hasClass('show')) {
+               $(this).next('div').addClass('show');
+               } else {
+               $(this).next('div').removeClass('show');
+               }
+           });
+       });
+       $(".navbar-toggler").mouseup(function(){
+           $(this).blur();
+       })
+   
+   
