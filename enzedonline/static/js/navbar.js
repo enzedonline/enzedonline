9f241e683461fdb73468499cbe7ada5429508a3f
Richard Allen <richard@enzedonline.com>
2021-08-03 14:55:58 +0200

    Search menu focus on search box

--- enzedonline/static/js/navbar.js
+++ enzedonline/static/js/navbar.js
@@ -1,17 +1,37 @@
 //  Dropdown menu aligns with right of button, lose focus after click
-$(document).ready(function() {
-    $('.dropright button').on("click", function(e) {
-        e.stopPropagation();
-        e.preventDefault();
+$(document).ready(function () {
+  $(".dropright button").on("click", function (e) {
+    e.stopPropagation();
+    e.preventDefault();
 
-        if (!$(this).next('div').hasClass('show')) {
-        $(this).next('div').addClass('show');
-        } else {
-        $(this).next('div').removeClass('show');
-        }
-    });
+    if (!$(this).next("div").hasClass("show")) {
+      $(this).next("div").addClass("show");
+    } else {
+      $(this).next("div").removeClass("show");
+    }
+  });
 });
-$('.navbar-toggler').mouseup(function(){
-    $(this).blur();
-})
- 
+$(".navbar-toggler").mouseup(function () {
+  $(this).blur();
+});
+
+respondToVisibility = function (element, callback) {
+  var options = {
+    root: document.documentElement,
+  };
+
+  var observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      callback(entry.intersectionRatio > 0);
+    });
+  }, options);
+
+  observer.observe(element);
+};
+
+respondToVisibility(document.getElementById("searchbox"), (visible) => {
+  if (visible) {
+    document.getElementById("searchbox").focus();
+    console.log("clicked");
+  }
+});
