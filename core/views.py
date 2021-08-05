6f650b26cfc71ddecdfda2d9ae187adc5f895544
Richard Allen <richard@enzedonline.com>
2021-08-05 20:12:27 +0200

    serach view logic, select searchtext on focus

--- core/views.py
+++ core/views.py
@@ -34,40 +34,43 @@ def search(request):
         s = get_search_backend(backend='default')
         search_results = s.search(search_query, Page.objects.live().specific().reverse())
 
-
         # Log the query so Wagtail can suggest promoted results
         Query.get(search_query).add_hit()
+
+        paginator = Paginator(search_results, 8)
+        requested_page = request.GET.get("page")
+
+        try:
+            search_results = paginator.page(requested_page)
+        except PageNotAnInteger:
+            search_results = paginator.page(1)
+        except EmptyPage:
+            search_results = paginator.page(paginator.num_pages)
+        
+        page_range = paginator_range(
+            requested_page=search_results.number,
+            last_page_num=paginator.num_pages,
+            wing_size=4
+        )
+        # Next two are needed as Django templates don't support accessing range properties
+        page_range_first = page_range[0]
+        page_range_last = page_range[-1]
+        
+        # Render template
+        return render(request, 'search/search_results.html', {
+            'search_query': search_query,
+            'query_string': '?query=' + search_query,
+            'search_results': search_results,
+            'page_range': page_range,
+            'page_range_first': page_range_first,
+            'page_range_last': page_range_last,
+        })        
     else:
-        search_query = ''
-        search_results = Page.objects.none()
-
-    paginator = Paginator(search_results, 8)
-    requested_page = request.GET.get("page")
-
-    try:
-        search_results = paginator.page(requested_page)
-    except PageNotAnInteger:
-        search_results = paginator.page(1)
-    except EmptyPage:
-        search_results = paginator.page(paginator.num_pages)
-    
-    page_range = paginator_range(
-        requested_page=search_results.number,
-        last_page_num=paginator.num_pages,
-        wing_size=4
-    )
-    # Next two are needed as Django templates don't support accessing range properties
-    page_range_first = page_range[0]
-    page_range_last = page_range[-1]
-    
-    # Render template
-    return render(request, 'search/search_results.html', {
-        'search_query': search_query,
-        'query_string': '?query=' + search_query,
-        'search_results': search_results,
-        'page_range': page_range,
-        'page_range_first': page_range_first,
-        'page_range_last': page_range_last,
-    })        
+        # Render template
+        return render(request, 'search/search_results.html', {
+            'search_query': '',
+            'query_string': '',
+            'search_results': Page.objects.none(),
+        })        
 
 
