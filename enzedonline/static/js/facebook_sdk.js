2d8e2c9009b0b4f02e11f1b3df9ca9815f6f8d5b
Richard Allen <richard@enzedonline.com>
2021-08-05 12:05:45 +0200

    seperate fb jdk from site js file

---
+++ enzedonline/static/js/facebook_sdk.js
@@ -0,0 +1,19 @@
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '492927945342914',
+      cookie     : true,
+      xfbml      : true,
+      version    : '11.0'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
