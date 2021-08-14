39ff79fac7e90cfd41a6ed6fcae2ccf037af688f
Richard Allen <richard@enzedonline.com>
2021-08-14 16:14:41 +0200

    Comments - show client time not server time

--- enzedonline/static/js/enzedonline.js
+++ enzedonline/static/js/enzedonline.js
@@ -1,4 +1,5 @@
-$("body").niceScroll({
+window.onload = function() {
+    $("body").niceScroll({
     cursorcolor:"#adb5bd",
     cursorwidth:"12px",
     cursorborderradius: "6px",
@@ -6,3 +7,12 @@ $("body").niceScroll({
     smoothscroll: true,  
     hidecursordelay: 1500,
 });
+}
+
+// Usage: document.getElementById("id").innerHTML = convertUTCDateToLocalDate(new Date('02 Aug 2021 09:58:22'));
+function convertUTCDateToLocalDate(date) {
+    local_date = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
+    const date_options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const time_options = { hour: '2-digit', minute: '2-digit', hour12: false };
+    return (local_date.toLocaleDateString(undefined, date_options) + ' ' + local_date.toLocaleTimeString(undefined, time_options));
+}
