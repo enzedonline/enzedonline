da27f4ac0f626352604ca1d3e662553d59eaf949
Richard Allen <richard@enzedonline.com>
2021-08-31 16:29:37 +0200

    remove print statement

--- core/templatetags/document_block_tags.py
+++ core/templatetags/document_block_tags.py
@@ -14,7 +14,6 @@ def document_list(sort_by, tag_list=None):
     docs = Document.objects.all()
     if tag_list:
         docs = docs.filter(tags__slug__in=tag_list.split(','))
-    print(docs.order_by('title'))
     if sort_by == 'title':
         return docs.order_by('title')
     return docs.order_by('created_at').reverse()
