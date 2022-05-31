acf9903865ee4f2944c44c17b3a17403b82243a1
Richard Allen <richard@enzedonline.com>
2022-05-31 16:37:39 +0200

    fix time offset

--- enzedonline/static/js/enzedonline.js
+++ enzedonline/static/js/enzedonline.js
@@ -1,6 +1,6 @@
 // Usage: document.getElementById("id").innerHTML = convertUTCDateToLocalDate(new Date('02 Aug 2021 09:58:22'));
 function convertUTCDateToLocalDate(date) {
-    local_date = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
+    local_date = new Date(date.getTime() + date.getTimezoneOffset() * 60000)
     const date_options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
     const time_options = { hour: '2-digit', minute: '2-digit', hour12: false };
     return (local_date.toLocaleDateString(undefined, date_options) + ' ' + local_date.toLocaleTimeString(undefined, time_options));
