ea7ca607a246773d9b086a6c0753f053f3944b8a
Richard Allen <richard@enzedonline.com>
2022-06-01 14:38:32 +0200

    remove inline script from head > analytics.js

---
+++ enzedonline/static/js/analytics.js
@@ -0,0 +1,22 @@
+// Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-708W2MWJN7");
+
+// Clarity tracking code for https://enzedonline.com/
+(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      (c[a].q = c[a].q || []).push(arguments);
+    };
+  t = l.createElement(r);
+  t.async = 1;
+  t.src = "https://www.clarity.ms/tag/" + i + "?ref=bwt";
+  y = l.getElementsByTagName(r)[0];
+  y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "8da1gexqzr");
