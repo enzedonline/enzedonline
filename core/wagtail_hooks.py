47011be34dd7f3d67017b5ba7b14625595682564
Richard Allen <richard@enzedonline.com>
2022-06-30 16:29:28 +0200

    hooks tweaks

--- core/wagtail_hooks.py
+++ core/wagtail_hooks.py
@@ -1,32 +1,15 @@
 from django.http import HttpResponse
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from wagtail import hooks
 from wagtail.admin.menu import MenuItem
 from wagtail.admin.rich_text.converters.html_to_contentstate import \
     InlineStyleElementHandler
-from wagtail.admin.rich_text.editors.draftail import \
-    features as draftail_features
-from wagtail import hooks
+from wagtail.admin.rich_text.editors.draftail.features import \
+    InlineStyleFeature
 
 from .utils import purge_page_cache_fragments
 
-
-@hooks.register('register_settings_menu_item')
-def register_refresh_cache_menu_item():
-    return MenuItem(_('Empty Cache'), reverse('refresh-page-cache'), classnames='icon icon-bin', order=1)
-
-@hooks.register('after_delete_page')
-def do_after_page_create(request, page):
-    purge_page_cache_fragments(page.slug)
-
-# === Enable after Wagtail 2.5 to have external links open in new tab ===
-# def external_link_handler(attrs):
-#     href = attrs["href"]
-#     return '<a href="%s" target="_blank" rel="noopener nofollower">' % escape(href)
-
-# @hooks.register('register_rich_text_features')
-# def register_external_link(features):
-#     features.register_link_type('external', external_link_handler)
 
 @hooks.register("register_rich_text_features")
 def register_small_styling(features):
@@ -46,7 +29,7 @@ def register_small_styling(features):
 
     # Step 3
     features.register_editor_plugin(
-        "draftail", feature_name, draftail_features.InlineStyleFeature(control)
+        "draftail", feature_name, InlineStyleFeature(control)
     )
 
     # Step 4
@@ -62,40 +45,6 @@ def register_small_styling(features):
     # This will register this feature with all richtext editors by default
     features.default_features.append(feature_name)
 
-@hooks.register("register_rich_text_features")
-def register_code_styling(features):
-    """Add the <code> to the richtext editor and page."""
-
-    # Step 1
-    feature_name = "code"
-    type_ = "CODE"
-    tag = "code"
-
-    # Step 2
-    control = {
-        "type": type_,
-        "label": "</>",
-        "description": "Code"
-    }
-
-    # Step 3
-    features.register_editor_plugin(
-        "draftail", feature_name, draftail_features.InlineStyleFeature(control)
-    )
-
-    # Step 4
-    db_conversion = {
-        "from_database_format": {tag: InlineStyleElementHandler(type_)},
-        "to_database_format": {"style_map": {type_: {"element": tag}}}
-    }
-
-    # Step 5
-    features.register_converter_rule("contentstate", feature_name, db_conversion)
-
-    # Step 6. This is optional
-    # This will register this feature with all richtext editors by default
-    features.default_features.append(feature_name)
-    
 @hooks.register("register_rich_text_features")
 def register_underline_styling(features):
     """Add the <u> to the richtext editor and page."""
@@ -114,7 +63,7 @@ def register_underline_styling(features):
 
     # Step 3
     features.register_editor_plugin(
-        "draftail", feature_name, draftail_features.InlineStyleFeature(control)
+        "draftail", feature_name, InlineStyleFeature(control)
     )
 
     # Step 4
@@ -148,7 +97,7 @@ def register_fontawesomeaward_styling(features):
 
     # Step 3
     features.register_editor_plugin(
-        "draftail", feature_name, draftail_features.InlineStyleFeature(control)
+        "draftail", feature_name, InlineStyleFeature(control)
     )
 
     # Step 4
@@ -156,9 +105,9 @@ def register_fontawesomeaward_styling(features):
         'from_database_format': {
             'i[class="fa-solid fa-award"]':
                 InlineStyleElementHandler(type_)},
-            'to_database_format': {
-                'style_map': {
-                 type_: 'i class="fa-solid fa-award"'}},
+        'to_database_format': {
+            'style_map': {
+                type_: 'i class="fa-solid fa-award"'}},
     }
 
     # Step 5
@@ -177,3 +126,12 @@ def serve_pdf(document, request):
     if request.GET.get('download', False) in [True, 'True', 'true']:
         response['Content-Disposition'] = 'attachment; ' + response['Content-Disposition']
     return response
+
+@hooks.register('register_settings_menu_item')
+def register_refresh_cache_menu_item():
+    return MenuItem(_('Empty Cache'), reverse('refresh-page-cache'), classnames='icon icon-bin', order=1)
+
+@hooks.register('after_delete_page')
+def do_after_delete_page(request, page):
+    purge_page_cache_fragments(page.slug)
+
