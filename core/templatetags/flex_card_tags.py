baa7863fbe0e5eaa37659e5fc9b0c773d549af87
Richard Allen <richard@enzedonline.com>
2022-07-17 16:15:15 +0200

    logic error in background test

--- core/templatetags/flex_card_tags.py
+++ core/templatetags/flex_card_tags.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 @register.simple_tag()
 def padding(border, background):
-    if background.find('bg-transparent') and not border:
+    if background.find('bg-transparent') != -1 and not border:
         return '0'
     else:
         return '4'
