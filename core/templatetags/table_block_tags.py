3881bc9235c7a51259f014b0146574eeeac7b3d2
Richard Allen <richard@enzedonline.com>
2022-08-25 21:32:03 +0200

    Fix non-string column headers alignment

--- core/templatetags/table_block_tags.py
+++ core/templatetags/table_block_tags.py
@@ -15,17 +15,31 @@ def render_html_table(table_block):
     df.replace(-999999, None, inplace=True)
     # Hide row numbers (index)
     dfs = df.style.hide()
-    # Align everything not an object (object=string) to the right
-    dfs = dfs.set_properties(subset=list(df.select_dtypes(exclude='object')), **{'text-align': 'right', 'padding-right': '1rem'})
     # Set missing values representation as empty string
     dfs = dfs.format(na_rep='')
     # Set decimal places for float. Redeclare na values for floats as format is not cumulative, it is replaced.
-    dfs = dfs.format(subset=list(df.select_dtypes(include='Float64')), precision=table_block['precision'], na_rep='')
+    dfs = dfs.format(
+        subset=list(df.select_dtypes(include='Float64')), 
+        precision=table_block['precision'], 
+        na_rep=''
+        )
     # If row headers, set formatting on 1st column (don't set to index, error thrown if not unique values)
     if table_block['row_headers']:
-        dfs = dfs.set_properties(subset=df.columns[0], **{'font-weight': 'bold', 'border-right-width': '0.1rem', 'border-right-color': 'var(--bs-dark)'})
-    # Set column headers left aligned
-    dfs = dfs.set_table_styles([ dict(selector='th', props=[('text-align', 'left')] ) ])
+        dfs = dfs.set_properties(
+            subset=df.columns[0], 
+            **{'font-weight': 'bold', 'border-right-width': '0.1rem', 'border-right-color': 'var(--bs-dark)'}
+            )
+    # Align everything not an object (object=string) to the right
+    dfs = dfs.set_properties(
+        subset=list(df.select_dtypes(exclude='object')), **{'text-align': 'right', 'padding-right': '0.7rem'}
+        )
+    # Set non-object column headers to right aligned
+    for column in list(df.select_dtypes(exclude='object')):
+        dfs = dfs.set_table_styles({
+            column: [{'selector': 'th', 
+                      'props': [('text-align', 'right'), ('padding-right', '0.7rem')]}]
+        }, overwrite=False)
+    # dfs = dfs.set_table_styles([ dict(selector='th', props=[('text-align', 'left')] ) ])
     # Add table classes and styles
     classes = f'class="table table-striped table-hover mx-auto w-{table_block["width"]}' + \
                 (" table-sm" if table_block['compact'] else '') + '"'
