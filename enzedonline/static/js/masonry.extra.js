54614fd7c704bd38bc043404bf4fb696421d1d3c
Richard Allen <richard@enzedonline.com>
2022-09-04 21:09:21 +0200

    masonry columns line up centrally

--- enzedonline/static/js/masonry.extra.js
+++ enzedonline/static/js/masonry.extra.js
@@ -1,3 +1,11 @@
-window.onload = function() {
+let timeout = false;
+const masonry_layout = () => {
     $('.grid').masonry('layout');
+}
+window.onload = function() {
+    masonry_layout;
+}
+window.onresize = function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(masonry_layout, 500);
 }
