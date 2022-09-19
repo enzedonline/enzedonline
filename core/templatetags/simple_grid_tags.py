a11752597bfd37d29d686ec9f4ef3fe58652cb6a
Richard Allen <richard@enzedonline.com>
2022-09-19 12:34:22 +0200

    get_masonry_layout->calc brkp's not col nums

--- core/templatetags/simple_grid_tags.py
+++ core/templatetags/simple_grid_tags.py
@@ -23,8 +23,27 @@ def get_column_layout(col_min, col_max):
              row-cols-xxl-{columns[5]}'
      
 @register.simple_tag()
-def get_masonry_layout(col_min, col_max):
-    # col_minmax = min and max number of columns to display
-    columns = get_column_number(col_min, col_max)
-    return dict(enumerate(columns))
+def get_masonry_options(col_min, col_max):
+    # col_min/max = min and max number of columns to display
+    minpx = 300 # px to start counting from - it just works
+    maxpx = 1400 # widest breakpoint, equivalenet to bootstrap xxl
+    steps = col_max - col_min # calculate number of changes
 
+    count = steps # loop counter
+    breakpoints = {}
+
+    for col in range(col_max, col_min-1, -1):
+        # loop from max to min, needed for creating the media css instructions in js
+        px = round(minpx + count * (maxpx-minpx)/steps)
+        breakpoints[f'min-width: {px}px'] = col
+        count = count - 1
+        
+    return {'breakpointCols': breakpoints, 'numCols': col_min}
+
+@register.simple_tag()
+def get_masonry_id(id):
+    # because Django can't create on-the-fly variables in function calls
+    return {
+        'grid': f'grid-{id}',
+        'options': f'grid-{id}-options'
+    }
