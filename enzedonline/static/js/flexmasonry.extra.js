9aae6777a5861597c42cf6a90a06637cd1a46790
Richard Allen <richard@enzedonline.com>
2022-09-19 12:32:43 +0200

    Add code for flexmasonry blocks

--- enzedonline/static/js/flexmasonry.extra.js
+++ enzedonline/static/js/flexmasonry.extra.js
@@ -1,3 +1,4 @@
+// code for the blog post masonry walls
 const flexMasonryInit = () => {
     FlexMasonry.init(
         '.grid',    
@@ -10,4 +11,34 @@ const flexMasonryInit = () => {
                 }
             }
         );
+}
+
+// code for the masonry blocks called by blocks/simple_card_grid_block.html
+// gridID - class used to call flexmasonry from the block - must be unique on the page
+// optionsID - id from JSON script used to store options for the block
+const initFlexMasonryBlock = (gridID, optionsID) => {
+    el = document.getElementsByClassName(gridID)[0]
+    // stop full container width element while flexmasonry loading
+    el.style.display = "flex";
+    resizeFlexMasonryBlock(gridID, optionsID);
+    // forced overflow error on hard refresh - call again 600ms after DOMContentLoaded
+    window.addEventListener('DOMContentLoaded', function() {
+        setTimeout(function() {resizeFlexMasonryBlock(gridID, optionsID); console.log('loaded');}, 600);
+    });
+    // multiple classes on same page not supported with flexmasonry.refresh() - call init on resize
+    window.addEventListener('resize', function() {
+        resizeFlexMasonryBlock(gridID, optionsID);
+    });
+    el.style.removeProperty("display");
+}
+
+const resizeFlexMasonryBlock = (gridID, optionsID) => {
+    const options = JSON.parse(document.getElementById(optionsID).textContent);
+    FlexMasonry.init(
+        '.' + gridID, 
+        {
+            responsive: true,
+            breakpointCols: options.breakpointCols, 
+        }
+    );
 }
