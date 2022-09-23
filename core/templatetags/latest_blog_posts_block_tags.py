8908510afbfbe0985a3ad68ee37c60b8d5977222
Richard Allen <richard@enzedonline.com>
2022-09-23 12:51:05 +0200

    change Locale to wagtail.models

--- core/templatetags/latest_blog_posts_block_tags.py
+++ core/templatetags/latest_blog_posts_block_tags.py
@@ -1,12 +1,18 @@
 from django import template
 from blog.models import BlogDetailPage
-from wagtail_localize.synctree import Locale
+from wagtail.models import Locale
+
 register = template.Library()
+
 
 @register.simple_tag()
 def get_latest_posts(post_count):
-    # .order_by('first_published_at') required to pick up both blog types in order 
-    posts = BlogDetailPage.objects.live().public().filter(locale_id=Locale.get_active().id).order_by('first_published_at').reverse()
+    # .order_by('first_published_at') required to pick up both blog types in order
+    posts = (
+        BlogDetailPage.objects.live()
+        .public()
+        .filter(locale_id=Locale.get_active().id)
+        .order_by("first_published_at")
+        .reverse()
+    )
     return posts[:post_count]
-
-    
