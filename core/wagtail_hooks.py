d87a496f2d60110bcaf57913e824d347e0fd093d
Richard Allen <richard@enzedonline.com>
2022-10-27 11:39:24 +0200

    draftail: drop small, add incr/decr font

--- core/wagtail_hooks.py
+++ core/wagtail_hooks.py
@@ -85,14 +85,29 @@ def register_fa_styling(features):
     )
 
 @hooks.register("register_rich_text_features")
-def register_small_styling(features):
+def register_smaller_styling(features):
     register_inline_styling(
         features=features,
-        feature_name='small',
-        type_='SMALL',
-        tag='small',
-        description='Small',
+        feature_name='smaller',
+        type_='SMALLER',
+        tag='span',
+        format='style="font-size:smaller"',
+        editor_style={'font-size':'smaller'},
+        description='Decrease Font',
         icon=DRAFTAIL_ICONS.decrease_font
+    )
+
+@hooks.register("register_rich_text_features")
+def register_larger_styling(features):
+    register_inline_styling(
+        features=features,
+        feature_name='larger',
+        type_='LARGER',
+        tag='span',
+        format='style="font-size:larger"',
+        editor_style={'font-size':'larger'},
+        description='Increase Font',
+        icon=DRAFTAIL_ICONS.increase_font
     )
 
 @hooks.register("register_rich_text_features")
