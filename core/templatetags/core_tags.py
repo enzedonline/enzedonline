6e778f9be71cd16e850629ae49c6921647f23f06
Richard Allen <richard@enzedonline.com>
2022-10-31 14:35:04 +0100

    Add canonical link to head

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -51,6 +51,13 @@ def robots(context):
     return mark_safe('<meta name="robots" content="index, follow, archive, imageindex, noodp, noydir, snippet, translate, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />')
 
 @register.simple_tag(takes_context=True)
+def canonical(context):
+    page = get_context_var_or_none(context, 'self')
+    if not page:
+        return ''
+    return mark_safe(f'<link rel="canonical" href="{page.full_url}" />')
+
+@register.simple_tag(takes_context=True)
 def get_cache_key_settings(context):
     page = get_context_var_or_none(context, 'self')
     if not page:
