db5f7939634f6faeba939dcddab6df125ae3c2f9
Richard Allen <richard@enzedonline.com>
2023-09-27 11:51:26 +1300

    Refactor

--- enzedonline/static/js/prism-django.js
+++ enzedonline/static/js/prism-django.js
@@ -1,14 +1,12 @@
-const processDjangoCodeBlock = (element) => {
-    const content = element.innerHTML;
+// Insert whitespace after "{%" and "{{" if the next character is not whitespace
+// Insert whitespace before "%}" and "}}" if the previous character is not whitespace
 
-    // Step 1: Insert whitespace after "{%" and "{{" if the next character is not whitespace
-    const step1Content = content.replace(/({%)(?!\s)/g, "$1 ")
-        .replace(/({{)(?!\s)/g, "$1 ");
-
-    // Step 2: Insert whitespace before "%}" and "}}" if the previous character is not whitespace
-    const step2Content = step1Content.replace(/(?<!\s)(%})/g, " $1")
+const processDjangoCodeBlock = (blockId) => {
+    const element = document.getElementById(`code-block-${blockId}`);
+    element.innerHTML = content
+        .replace(/({%)(?!\s)/g, "$1 ")
+        .replace(/({{)(?!\s)/g, "$1 ")
+        .replace(/(?<!\s)(%})/g, " $1")
         .replace(/(?<!\s)(}})/g, " $1");
-
-    // Update the inner HTML of the element with the processed content
-    element.innerHTML = step2Content;
 };
+
