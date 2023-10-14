97b95b319042bc7607265b51db110b45f036315b
Richard Allen <richard@enzedonline.com>
2023-10-14 16:25:26 +1300

    remove unused pre wag3 code

--- core/edit_handlers.py
+++ core/edit_handlers.py
@@ -19,24 +19,3 @@ class RegexPanel(FieldPanel):
             self.form.fields[self.field_name].__setattr__('pattern', self.panel.pattern)
 
         field_template_name = "edit_handlers/regex_panel_field.html"
-
-## Wagtail 2.x
-#
-# class RegexPanel(FieldPanel):
-#     def __init__(self, field_name, pattern, *args, **kwargs):
-#         self.pattern = pattern
-#         super().__init__(field_name, *args, **kwargs)
-
-#     def clone_kwargs(self):
-#         kwargs = super().clone_kwargs()
-#         kwargs.update(
-#             field_name=self.field_name,
-#             pattern=self.pattern,
-#         )
-#         return kwargs
-
-#     field_template = "edit_handlers/regex_panel_field.html"
-
-#     def on_form_bound(self):
-#         self.form.fields[self.field_name].__setattr__('pattern',self.pattern)
-#         super().on_form_bound()
