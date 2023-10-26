986882689862caf89e0f584e23f7467c07fdcbda
Richard Allen <richard@enzedonline.com>
2023-10-26 14:29:04 +1300

    Exit if no results. Assign ls result to variable.

--- purge_migrations.sh
+++ purge_migrations.sh
@@ -8,13 +8,19 @@ fi
 app="$1"
 start_number="$2"
 
-echo "The following migration files will be deleted:"
-ls "$app/migrations" | grep -E '^[0-9]{4}.*\.py$' | awk -v app="$app" -v start_number="$start_number" '$1 > start_number {print app "/migrations/" $1}' 
+results=$(ls "$app/migrations" | grep -E '^[0-9]{4}.*\.py$' | awk -v app="$app" -v start_number="$start_number" '$1 > start_number {print app "/migrations/" $1}')
 
-read -p $'\e[1;31mWARNING: \e[1mDo you want to proceed? (y/n): \e[0m' answer
+if [ -z "$results" ]; then
+    echo "No matching migration files found to purge."
+    exit 0
+fi
+
+echo "$results"
+
+read -p $'\e[1;31mWARNING: \e[1mThe migration files listed above will be deleted. Do you want to proceed? (y/n): \e[0m' answer
 
 if [ "$answer" == "y" ]; then
     echo "Deleting files..."
-    ls "$app/migrations" | grep -E '^[0-9]{4}.*\.py$' | awk -v app="$app" -v start_number="$start_number" '$1 > start_number {print app "/migrations/" $1}' | xargs rm
+    echo "$results" | xargs rm
     echo "Files deleted."
 fi
