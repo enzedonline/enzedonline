f34db61db465a39bb92551694370ab83417ce765
Richard Allen <richard@enzedonline.com>
2023-10-26 14:06:27 +1300

    Full path to rm

--- purge_migrations.sh
+++ purge_migrations.sh
@@ -9,12 +9,12 @@ app="$1"
 start_number="$2"
 
 echo "The following migration files will be deleted:"
-ls "$app/migrations" | grep -E '^[0-9]{4}.*\.py$' | awk -v start_number="$start_number" '$1 > start_number'
+ls "$app/migrations" | grep -E '^[0-9]{4}.*\.py$' | awk -v app="$app" -v start_number="$start_number" '$1 > start_number {print app "/migrations/" $1}' 
 
 read -p $'\e[1;31mWARNING: \e[1mDo you want to proceed? (y/n): \e[0m' answer
 
 if [ "$answer" == "y" ]; then
     echo "Deleting files..."
-    ls "$app/migrations" | grep -E '^[0-9]{4}.*\.py$' | awk -v start_number="$start_number" '$1 > start_number' | xargs rm
+    ls "$app/migrations" | grep -E '^[0-9]{4}.*\.py$' | awk -v app="$app" -v start_number="$start_number" '$1 > start_number {print app "/migrations/" $1}' | xargs rm
     echo "Files deleted."
 fi
