ebdf0e1619a0433400f4caff5a0f00e16d660c96
Richard Allen <richard@enzedonline.com>
2023-11-17 14:38:42 +1300

    Test finish

--- userauth/forms.py
+++ userauth/forms.py
@@ -43,7 +43,7 @@ class SignupForm(forms.Form):
         user.last_name = self.cleaned_data['last_name']
         user.display_name = display_name or f'{user.first_name} {user.last_name}'
         user.website = self.cleaned_data['website']
-        # user.save()
+        user.save()
 
 class WagtailUserCreationForm(UserCreationForm):
     display_name = forms.CharField(max_length=30, label=_("Display name"), help_text=_("Will be shown e.g. when commenting."))
