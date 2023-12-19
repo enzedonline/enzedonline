b9a648de89302d6ff6840406b8ee80aafe319ff0
Richard Allen <richard@enzedonline.com>
2023-12-19 16:47:16 +1300

    login and signup now sue django-recaptcha

--- userauth/forms.py
+++ userauth/forms.py
@@ -5,43 +5,19 @@ from django.conf import settings
 from django.forms import ModelForm
 from django.utils.translation import gettext_lazy as _
 from wagtail.users.forms import UserCreationForm, UserEditForm
-
+from django_recaptcha.fields import ReCaptchaField
+from django_recaptcha.widgets import ReCaptchaV3
 from .models import CustomUser
 
-def recaptcha_is_valid(token):
-    request = requests.post(
-        'https://www.recaptcha.net/recaptcha/api/siteverify', 
-        data={
-            'secret': settings.RECAPTCHA_PRIVATE_KEY,
-            'response': token
-        }
-    )
-    result = request.json()
-    return (result['success'] and result['score'] >= settings.RECAPTCHA_REQUIRED_SCORE)
-
 class LoginForm(BaseLoginForm):
-    recaptcha = forms.CharField(label="", widget=forms.HiddenInput())
-
-    def clean(self):
-        if recaptcha_is_valid(self.data.get('recaptcha')):
-            cleaned_data = super().clean()
-            return cleaned_data
-        else:
-            self.add_error('', _("Invalid recaptcha. Please try again."))
+    recaptcha = ReCaptchaField(widget=ReCaptchaV3(action='login'))
     
 class SignupForm(BaseSignupForm):
     first_name = forms.CharField(max_length=30, label=_("First name"))
     last_name = forms.CharField(max_length=30, label=_("Last name"))
     display_name = forms.CharField(max_length=30, label=_("Display name"), help_text=_("Will be shown e.g. when commenting."))
     website = forms.CharField(max_length=30, label=_("Website"), required=False)
-    recaptcha = forms.CharField(label="", widget=forms.HiddenInput())
-
-    def clean(self):
-        if recaptcha_is_valid(self.data.get('recaptcha')):
-            cleaned_data = super().clean()
-            return cleaned_data
-        else:
-            self.add_error('', _("Invalid recaptcha. Please try again."))
+    recaptcha = ReCaptchaField(widget=ReCaptchaV3(action='signup'))
         
     def signup(self, request, user):
         display_name = self.cleaned_data['display_name']
