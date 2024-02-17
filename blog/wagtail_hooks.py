017b591503cbb12ded78481450ff7b9f0a2821c4
Richard Allen <richard@enzedonline.com>
2024-02-17 16:22:19 +1300

    update unused_tags and wordcount

--- blog/wagtail_hooks.py
+++ blog/wagtail_hooks.py
@@ -3,29 +3,37 @@ from django.db.models import Count
 from taggit.models import Tag
 from wagtail import hooks
 
-from .models import PersonalBlogDetailPage, TechBlogDetailPage
+from .models import BlogDetailPage
 
 
 def purge_unused_tags():
-    tags = Tag.objects.annotate(ntech=Count('blog_techblogpagetag_items')).filter(ntech=0)
-    tags.annotate(npersonal=Count('blog_personalblogpagetag_items')).filter(npersonal=0).delete()
+    unused_tags = Tag.objects.annotate(
+        ntech=Count('blog_techblogpagetag_items')
+    ).annotate(
+        npersonal=Count('blog_personalblogpagetag_items')
+    ).filter(ntech=0).filter(npersonal=0).values('id')
+    
+    if unused_tags:
+        unused_tag_ids = {tag['id'] for tag in unused_tags}    
+        draft_tags=set({})
+        pages_with_drafts=BlogDetailPage.objects.filter(has_unpublished_changes=True)
+        for page in pages_with_drafts:
+            draft=page.get_latest_revision_as_object()
+            tag_ids = draft.tags.values('id')
+            draft_tags|= {tag['id'] for tag in tag_ids}
+        unused_tag_ids = unused_tag_ids.difference(draft_tags)
+        Tag.objects.filter(id__in=unused_tag_ids).delete()
 
 @hooks.register('after_delete_page')
-def do_after_delete_page(request, page):
-    index_type = page.__class__
-    if(index_type== PersonalBlogDetailPage or index_type== TechBlogDetailPage):
-        purge_unused_tags()
-
 @hooks.register('after_publish_page')
-def do_after_publish_page(request, page):
-    index_type = page.__class__
-    if(index_type== PersonalBlogDetailPage or index_type== TechBlogDetailPage):
+def do_after_publish_delete_page(request, page):
+    if issubclass(page.specific_class, BlogDetailPage):
         purge_unused_tags()
 
 @hooks.register("after_create_page")
 @hooks.register("after_edit_page")
 def get_wordcount(request, page):
-    if page.specific_class in [TechBlogDetailPage, PersonalBlogDetailPage]:
+    if issubclass(page.specific_class, BlogDetailPage):
         try:
             page.wordcount = page.get_wordcount()
             if page.has_unpublished_changes:
@@ -33,5 +41,7 @@ def get_wordcount(request, page):
             else:
                 page.save()
         except Exception as e:
-            print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")       
-            messages.error(request, 'There was a problem generating the word count')
+            print(f"{type(e).__name__} at line {
+                  e.__traceback__.tb_lineno} of {__file__}: {e}")
+            messages.error(
+                request, 'There was a problem generating the word count')
