6c91b29ad6a63de9e73a2b9069a960bd8c729908
Richard Allen <richard@enzedonline.com>
2024-03-18 11:14:09 +1300

    sitemap - use site.root_page

--- core/views.py
+++ core/views.py
@@ -18,20 +18,19 @@ def refresh_page_cache(r):
     except:
         messages.error(r, _('There was a problem refreshing the Page Cache'))
     return redirect(r.META['HTTP_REFERER'])
-    
-class RobotsView(TemplateView):
 
+
+class RobotsView(TemplateView):
     content_type = 'text/plain'
 
     def get_template_names(self):
         return 'robots.txt'
 
+
 def sitemap(request):
     site = Site.find_for_request(request)
-    root_page = Page.objects.defer_streamfields().get(id=site.root_page_id)
-
     urlset = []
-    for locale_home in root_page.get_translations(inclusive=True).defer_streamfields():
+    for locale_home in site.root_page.get_translations(inclusive=True).defer_streamfields():
         for child_page in locale_home.get_descendants(inclusive=True).defer_streamfields().live().public().specific():
             urlset.append(child_page.get_sitemap_urls())
     try:
@@ -41,13 +40,13 @@ def sitemap(request):
     last_modified = max([x['lastmod'] for x in urlset])
 
     return TemplateResponse(
-        request, 
-        template='sitemap.xml', 
+        request,
+        template='sitemap.xml',
         context={'urlset': urlset},
         content_type='application/xml',
         headers={
-            "X-Robots-Tag": "noindex, noodp, noarchive", 
+            "X-Robots-Tag": "noindex, noodp, noarchive",
             "last-modified": http_date(last_modified.timestamp()),
             "vary": "Accept-Encoding",
-            }
-        )
+        }
+    )
