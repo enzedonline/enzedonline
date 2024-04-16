5c7e6ec1c832bf01bbf1c561001e91634089bd7a
Richard Allen <richard@enzedonline.com>
2024-04-16 12:57:54 +1200

    Update signup/update forms

--- userauth/forms.py
+++ userauth/forms.py
@@ -1,13 +1,17 @@
 import requests
-from allauth.account.forms import LoginForm as BaseLoginForm, SignupForm as BaseSignupForm
+from allauth.account.forms import LoginForm as BaseLoginForm
+from allauth.account.forms import SignupForm as BaseSignupForm
 from django import forms
 from django.conf import settings
 from django.forms import ModelForm
 from django.utils.translation import gettext_lazy as _
-from wagtail.users.forms import UserCreationForm, UserEditForm
+from django_countries.fields import CountryField
 from django_recaptcha.fields import ReCaptchaField
 from django_recaptcha.widgets import ReCaptchaV3
+from wagtail.users.forms import UserCreationForm, UserEditForm
+
 from .models import CustomUser
+
 
 class LoginForm(BaseLoginForm):
     recaptcha = ReCaptchaField(widget=ReCaptchaV3(action='login'))
@@ -17,6 +21,7 @@ class SignupForm(BaseSignupForm):
     last_name = forms.CharField(max_length=30, label=_("Last name"))
     display_name = forms.CharField(max_length=30, label=_("Display name"), help_text=_("Will be shown e.g. when commenting."))
     website = forms.CharField(max_length=30, label=_("Website"), required=False)
+    country = CountryField().formfield(required=False, widget=forms.Select())
     recaptcha = ReCaptchaField(widget=ReCaptchaV3(action='signup'))
         
     def signup(self, request, user):
@@ -25,12 +30,13 @@ class SignupForm(BaseSignupForm):
         user.last_name = self.cleaned_data['last_name']
         user.display_name = display_name or f'{user.first_name} {user.last_name}'
         user.website = self.cleaned_data['website']
+        user.country = self.cleaned_data['country']
         user.save()
 
 class CustomUserUpdateForm(ModelForm):
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name', 'display_name', 'city', 'country', 'website']
+        fields = ['first_name', 'last_name', 'display_name', 'website', 'country']
 
 class WagtailUserCreationForm(UserCreationForm):
     display_name = forms.CharField(max_length=30, label=_("Display name"), help_text=_("Will be shown e.g. when commenting."))
