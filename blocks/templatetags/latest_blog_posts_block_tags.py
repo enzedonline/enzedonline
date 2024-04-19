714808de7b0b0c8a4ba57aa98c6b8982407ac90d
Richard Allen <richard@enzedonline.com>
2024-04-20 11:51:03 +1200

    mroe efficient orm query

--- blocks/templatetags/latest_blog_posts_block_tags.py
+++ blocks/templatetags/latest_blog_posts_block_tags.py
@@ -7,12 +7,11 @@ register = template.Library()
 
 @register.simple_tag()
 def get_latest_posts(post_count):
-    # .order_by('first_published_at') required to pick up both blog types in order
+    # .order_by('-first_published_at') required to pick up both blog types in order
     posts = (
         BlogDetailPage.objects.live().defer_streamfields()
         .public()
         .filter(locale_id=Locale.get_active().id)
-        .order_by("first_published_at")
-        .reverse()
+        .order_by("-first_published_at")[:post_count]
     )
-    return posts[:post_count]
+    return posts
