19e406a43783bd7788c56f6d4a1ef302b768e2d2
rich <richard@enzedonline.com>
2024-07-08 11:07:06 +1200

    Update MathJax to svg path

--- enzedonline/static/js/enzedonline.js
+++ enzedonline/static/js/enzedonline.js
@@ -89,7 +89,7 @@ const loadMathJax = () => {
     }
     let script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-svg.js';
     script.id = 'MathJax-script';
     script.async = true;
     document.head.appendChild(script);
