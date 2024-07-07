afc9a0d1db3c92c12b41c8901ee081febf5335ab
rich <richard@enzedonline.com>
2024-07-07 14:11:48 +1200

    test remove polyfill.io from mathjax

--- enzedonline/static/js/enzedonline.js
+++ enzedonline/static/js/enzedonline.js
@@ -88,10 +88,10 @@ const loadMathJax = () => {
       };
     }
     let script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://polyfill.io/v3/polyfill.min.js?features=es6';
-    document.head.appendChild(script);
-    script = document.createElement('script');
+    // script.type = 'text/javascript';
+    // script.src = 'https://polyfill.io/v3/polyfill.min.js?features=es6';
+    // document.head.appendChild(script);
+    // script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
     script.id = 'MathJax-script';
