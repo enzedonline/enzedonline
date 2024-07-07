fd3fc6afbd8ce87bc143b4df04b76d75e7ebb8a9
rich <richard@enzedonline.com>
2024-07-07 14:22:33 +1200

    Update mathjax source with polyfill.io fix

--- enzedonline/static/js/enzedonline.js
+++ enzedonline/static/js/enzedonline.js
@@ -89,7 +89,7 @@ const loadMathJax = () => {
     }
     let script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+    script.src = "https://cdnjs.cloudflare.com/polyfill/v3/polyfill.min.js?features=es6";
     script.id = 'MathJax-script';
     script.async = true;
     document.head.appendChild(script);
