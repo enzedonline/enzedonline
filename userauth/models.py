78a8262edce749072019114be43b2586d79f5bcd
rich <richard@enzedonline.com>
2024-11-16 16:34:45 +1300

    +icons

--- userauth/models.py
+++ userauth/models.py
@@ -4,9 +4,14 @@ import string
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
+from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
 from django_countries.fields import CountryField
 from django_extensions.db.fields import AutoSlugField
+from django.conf import settings
+from django.utils.http import urlencode
+from wagtail.coreutils import safe_md5
+from site_settings.models import DefaultUserIcon
 
 class CustomUser(AbstractUser):
     display_name = models.CharField(
@@ -35,4 +40,56 @@ class CustomUser(AbstractUser):
          
     def save(self, *args, **kwargs):
         if not self.display_name: self.display_name=f'{self.first_name} {self.last_name}'
+        self.icon_url = self.get_absolute_url()
         super().save(*args, **kwargs)
+
+    @cached_property
+    def default_menu_icon(self):
+        try:
+            default = settings.WAGTAILADMIN_BASE_URL + DefaultUserIcon.load().menu_icon.get_rendition('original').url
+        except:
+            default = "mp"
+        return default
+        
+    @cached_property
+    def default_comments_icon(self):
+        try:
+            default = settings.WAGTAILADMIN_BASE_URL + DefaultUserIcon.load().comments_icon.get_rendition('original').url
+        except:
+            default = "mp"
+        return default
+
+    @property
+    def menu_icon(self):
+        return self.get_user_icon()
+    
+    @property
+    def comments_icon(self):
+        return self.get_user_icon(isMenuIcon=False)
+        
+
+    def get_user_icon(self, isMenuIcon=True, size=50):
+        default = self.default_menu_icon if isMenuIcon else self.default_comments_icon
+        size = (
+            int(size) * 2
+        )
+        gravatar_provider_url = getattr(
+            settings, "WAGTAIL_GRAVATAR_PROVIDER_URL", "//www.gravatar.com/avatar"
+        )
+        try:
+            email = self.email
+        except:
+            email = False
+            
+        if (not email) or (gravatar_provider_url is None):
+            return None
+
+        email_bytes = email.lower().encode("utf-8")
+        hash = safe_md5(email_bytes, usedforsecurity=False).hexdigest()
+        gravatar_url = "{gravatar_provider_url}/{hash}?{params}".format(
+            gravatar_provider_url=gravatar_provider_url.rstrip("/"),
+            hash=hash,
+            params=urlencode({"s": size, "d": default}),
+        )
+
+        return gravatar_url
