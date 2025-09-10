315af87e0fa1ad73f7c4ae31c217b3dbb1492eca
rich <richard@enzedonline.com>
2025-09-10 13:03:35 +1200

    Refactor TestContactReceiptView to dynamically include site hostname and brand banner in email context

--- contact/views.py
+++ contact/views.py
@@ -1,15 +1,19 @@
 from django.views.generic import TemplateView
-from site_settings.models import EmailSignature
 from wagtail.images.models import Image
-   
+from wagtail.models import Site
+
+from site_settings.models import Brand, EmailSignature
+
 
 class TestContactReceiptView(TemplateView):
     template_name = 'contact/receipt-email.html' 
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['body'] = {'receipt_email_headline': 'Thank you for contacting Enzed Online', 'receipt_email_content': '<p>Your message is important to me, I will endeavour to respond at the first available opportunity.</p><p>Regards,<br>Richard @ Enzed Online</p>'}
-        context['heading_image'] = Image.objects.get(id=48)
+        site = Site.find_for_request(self.request)
+        context['body'] = {'receipt_email_headline': f'Thank you for contacting {site.hostname}', 'receipt_email_content': '<p>Your message is important to me, I will endeavour to respond at the first available opportunity.</p><p>Regards,<br>Richard @ Enzed Online</p>'}
+        brand = Brand.for_request(self.request)
+        context['heading_image'] = brand.banner
         context['footer'] = EmailSignature.objects.first()
-        context['base_url'] = 'localhost:8000'
+        context['base_url'] = f'{site.root_url}/'
         return context
