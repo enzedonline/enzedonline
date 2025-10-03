0f94951deae676cfce9d4a1913c3b16b8d938360
rich <richard@enzedonline.com>
2025-10-03 16:00:51 +1300

    +FlexCard

--- blocks/streamblocks/simple.py
+++ blocks/streamblocks/simple.py
@@ -1,6 +1,6 @@
 from wagtail.blocks import StreamBlock
 
-from ..models import (CallToActionCard, ImageBlock, SimpleCard, RichTextBlock,
+from ..models import (CallToActionCard, ImageBlock, FlexCard, SimpleCard, RichTextBlock,
                       SpacerStaticBlock)
 
 
@@ -8,5 +8,6 @@ class SimpleStreamBlock(StreamBlock):
     rich_text_block = RichTextBlock()
     image_block = ImageBlock()
     simple_card = SimpleCard()
+    flex_card = FlexCard()
     call_to_action_card = CallToActionCard()
     spacer_block = SpacerStaticBlock()
